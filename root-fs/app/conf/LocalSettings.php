56b13ae7c96b47474dd134010d418d9a08d46da8
Bosper-the-Magician <fabian.spinar@hallowelt.com>
2025-02-24 13:58:07 +0100

    init

--- root-fs/app/conf/LocalSettings.php
+++ root-fs/app/conf/LocalSettings.php
@@ -3,20 +3,11 @@ if ( !defined( 'MEDIAWIKI' ) ) {
 	exit;
 }
 
-### Dynamic assembly of $GLOBALS['wgServer']
-$protocol = getenv( 'WIKI_PROTOCOL' ) ?: 'https';
-$host = getenv( 'WIKI_HOST' ) ?: 'localhost';
-$portSuffix = getenv( 'WIKI_PORT' ) ? ':' . getenv( 'WIKI_PORT' ) : ':443';
-if ( $protocol === 'http' && $portSuffix === ':80' ) {
-	$portSuffix = '';
-} elseif ( $protocol === 'https' && $portSuffix === ':443' ) {
-	$portSuffix = '';
-}
-$GLOBALS['wgServer'] = "$protocol://$host{$portSuffix}";
-unset( $protocol );
-unset( $host );
-unset( $portSuffix );
-### end
+$GLOBALS['wgServer'] = bsAssembleURL(
+	[ 'WIKI_PROTOCOL', 'https' ],
+	[ 'WIKI_HOST', 'localhost' ],
+	[ 'WIKI_PORT', '443' ]
+);
 
 $GLOBALS['wgSitename'] = getenv( 'WIKI_NAME' ) ?: 'BlueSpice';
 $GLOBALS['wgScriptPath'] = "/w";
@@ -35,8 +26,6 @@ $GLOBALS['wgDBuser'] = getenv( 'DB_USER' ) ?: 'bluespice';
 $GLOBALS['wgDBpassword'] = getenv( 'DB_PASS' );
 $GLOBALS['wgDBprefix'] = getenv( 'DB_PREFIX' ) ?: '';
 $GLOBALS['wgDBTableOptions'] = "ENGINE=InnoDB, DEFAULT CHARSET=binary";
-$GLOBALS['wgSharedTables'][] = "actor";
-$GLOBALS['wgMainCacheType'] = CACHE_ACCEL;
 $cacheHost = getenv( 'CACHE_HOST' ) ?: 'cache';
 $cachePort = getenv( 'CACHE_PORT' ) ?: '11211';
 $GLOBALS['wgMemCachedServers'] = [ "$cacheHost:$cachePort" ];
@@ -70,6 +59,25 @@ $GLOBALS['wgSMTP'] = [
 	'username' => getenv( 'SMTP_USER' ),
 	'password' => getenv( 'SMTP_PASS' ),
 ];
+if ( getenv( 'AV_HOST' ) ) {
+	$GLOBALS['wgAntivirusSetup'] = [
+		'clamav' => [
+			'command' => 'clamdscan --no-summary',
+			'codemap' => [
+				"0" => AV_NO_VIRUS,
+				"1" => AV_VIRUS_FOUND,
+				"52" => AV_SCAN_ABORTED,
+				"*" => AV_SCAN_FAILED,
+			],
+			'messagepattern' => '/.*?:(.*)/sim',
+		],
+	];
+	$GLOBALS['wgAntivirus'] = 'clamav';
+	$GLOBALS['wgAntivirusRequired'] = true;
+}
+
+$GLOBALS['wgOAuth2PrivateKey'] = '/data/bluespice/oauth_private.key';
+$GLOBALS['wgOAuth2PublicKey'] = '/data/bluespice/oauth_public.key';
 
 if ( getenv( 'DEV_WIKI_DEBUG' ) ) {
 	#$GLOBALS['wgDebugToolbar'] = true;
@@ -86,9 +94,26 @@ if ( getenv( 'DEV_WIKI_DEBUG_LOGCHANNELS' ) ) {
 	}
 	unset( $logChannels );
 }
+define( 'BSROOTDIR', '/data/bluespice/extensions/BlueSpiceFoundation' );
 
-// Taken from `extensions/BlueSpiceWikiFarm/SimpleFarmer/src/Dispatcher.php`
-// Not all of this may be required
+if ( getenv( 'EDITION' ) === 'farm' ) {
+	$GLOBALS['wgWikiFarmConfig_instanceDirectory'] = '/data/bluespice/farm-instances/';
+	$GLOBALS['wgWikiFarmConfig_archiveDirectory'] = '/data/bluespice/farm-archives/';
+	$GLOBALS['wgWikiFarmConfig_dbAdminUser'] = getenv( 'DB_ROOT_USER' ) ?: 'root';
+	$GLOBALS['wgWikiFarmConfig_dbAdminPassword'] = getenv( 'DB_ROOT_PASS' ) ?: $GLOBALS['wgDBpassword'];
+	$GLOBALS['wgWikiFarmConfig_LocalSettingsAppendPath'] = "$IP/LocalSettings.BlueSpice.php";
+	$GLOBALS['wgSharedDB'] = $GLOBALS['wgDBname'];
+	$GLOBALS['wgSharedPrefix'] = $GLOBALS['wgDBprefix'];
+	$GLOBALS['wgSharedTables'] = [ 'bs_translationtransfer_translations' ];
+}
+
+require_once '/data/bluespice/pre-init-settings.php';
+if ( getenv( 'EDITION' ) === 'farm' ) {
+	require_once "$IP/extensions/BlueSpiceWikiFarm/WikiFarm.setup.php";
+}
+else {
+	// Taken from `extensions/BlueSpiceWikiFarm/src/Dispatcher.php`
+	// Not all of this may be required
 $GLOBALS['wgUploadDirectory'] = "/data/bluespice/images";
 $GLOBALS['wgReadOnlyFile'] = "{$GLOBALS['wgUploadDirectory']}/lock_yBgMBwiR";
 $GLOBALS['wgFileCacheDirectory'] = "{$GLOBALS['wgUploadDirectory']}/cache";
@@ -99,20 +124,7 @@ define( 'BSDATADIR', "/data/bluespice/extensions/BlueSpiceFoundation/data" ); //
 define( 'BS_DATA_DIR', "{$GLOBALS['wgUploadDirectory']}/bluespice" ); //Future
 define( 'BS_CACHE_DIR', "{$GLOBALS['wgFileCacheDirectory']}/bluespice" );
 define( 'BS_DATA_PATH', "{$GLOBALS['wgUploadPath']}/bluespice" );
-
-$GLOBALS['bsgSimpleFarmer_instanceDirectory'] = '/data/bluespice/_sf_instances/';
-$GLOBALS['bsgSimpleFarmer_archiveDirectory'] = '/data/bluespice/_sf_archives/';
-$GLOBALS['bsgSimpleFarmer_dbAdminUser'] = getenv( 'DB_ROOT_USER' ) ?: 'root';
-$GLOBALS['bsgSimpleFarmer_dbAdminPassword'] = getenv( 'DB_ROOT_PASS' ) ?: $GLOBALS['wgDBpassword'];
-require_once '/data/bluespice/pre-init-settings.php';
-if ( getenv( 'EDITION' ) === 'farm' ) {
-	// We must store L10N cache file of ROOT_WIKI and INSTANCEs independently, as they have different extensions enabled,
-	// which otherwise causes the cache to be invalidated all the time.
-	$GLOBALS['wgLocalisationCacheConf']['storeDirectory'] = '/tmp/cache/l10n-instances';
-	require_once "$IP/extensions/BlueSpiceWikiFarm/BlueSpiceWikiFarm.php";
-}
-else {
-	require_once "$IP/LocalSettings.BlueSpice.php";
+require_once "$IP/LocalSettings.BlueSpice.php";
 }
 
 $GLOBALS['wgArticlePath'] = '/wiki/$1';
@@ -120,24 +132,23 @@ if ( getenv( 'EDITION' ) === 'farm' ) {
 	if( FARMER_IS_ROOT_WIKI_CALL === false ) {
 		$GLOBALS['wgArticlePath'] = '/' . FARMER_CALLED_INSTANCE . '/wiki/$1';
 		$GLOBALS['wgWebDAVBaseUri'] = '/' . FARMER_CALLED_INSTANCE . '/webdav/';
+				// We must store L10N cache file of ROOT_WIKI and INSTANCEs independently, as they have different extensions enabled,
+		// which otherwise causes the cache to be invalidated all the time.
+		$GLOBALS['wgLocalisationCacheConf']['storeDirectory'] = '/tmp/cache/l10n-instances';
 	}
 }
 
-wfLoadExtension( 'BlueSpiceExtendedSearch' );
-$GLOBALS['bsgOverrideESBackendHost'] = getenv( 'SEARCH_HOST' ) ?: 'search';
-$GLOBALS['bsgOverrideESBackendPort'] = getenv( 'SEARCH_PORT' ) ?: '9200';
-$GLOBALS['bsgOverrideESBackendTransport'] = getenv( 'SEARCH_PROTOCOL' ) ?: 'http';
+$GLOBALS['bsgESBackendHost'] = getenv( 'SEARCH_HOST' ) ?: 'search';
+$GLOBALS['bsgESBackendPort'] = getenv( 'SEARCH_PORT' ) ?: '9200';
+$GLOBALS['bsgESBackendTransport'] = getenv( 'SEARCH_PROTOCOL' ) ?: 'http';
 
-wfLoadExtension( 'BlueSpiceUEModulePDF' );
-$pdfProtocol = getenv( 'PDF_PROTOCOL' ) ?: 'http';
-$pdfHost = getenv( 'PDF_HOST' ) ?: 'pdf';
-$pdfPort = getenv( 'PDF_PORT' ) ?: '8080';
-$GLOBALS['bsgOverrideUEModulePDFPdfServiceURL'] = "$pdfProtocol://$pdfHost:$pdfPort/BShtml2PDF";
-unset( $pdfProtocol );
-unset( $pdfHost );
-unset( $pdfPort );
+$GLOBALS['wgPDFCreatorOpenHtml2PdfServiceUrl'] = bsAssembleURL(
+	[ 'PDF_PROTOCOL', 'http' ],
+	[ 'PDF_HOST', 'pdf' ],
+	[ 'PDF_PORT', '8080' ]
+);
+$GLOBALS['wgPDFCreatorOpenHtml2PdfServiceUrl'] .= '/Html2PDF/v1';
 
-wfLoadExtension( 'PdfHandler' );
 $GLOBALS['wgPdfProcessor'] = '/usr/bin/gs';
 $GLOBALS['wgPdfPostProcessor'] = $GLOBALS['wgImageMagickConvertCommand'];
 $GLOBALS['wgPdfInfo'] = '/usr/bin/pdfinfo';
@@ -148,16 +159,14 @@ if ( getenv( 'EDITION' ) !== 'free' ) {
 	$GLOBALS['wgDrawioEditorBackendUrl'] = $GLOBALS['wgServer'] . '/_diagram/';
 }
 
-$formulaProtocol = getenv( 'FORMULA_PROTOCOL' ) ?: 'http';
-$formulaHost = getenv( 'FORMULA_HOST' ) ?: 'formula';
-$formulaPort = getenv( 'FORMULA_PORT' ) ?: '10044';
 $GLOBALS['wgMathoidCli'] = [
 	'/app/bin/mathoid-remote',
-	"$formulaProtocol://$formulaHost:$formulaPort",
+	bsAssembleURL(
+		[ 'FORMULA_PROTOCOL', 'http' ],
+		[ 'FORMULA_HOST', 'formula' ],
+		[ 'FORMULA_PORT', '10044' ]
+	),
 ];
-unset( $formulaProtocol );
-unset( $formulaHost );
-unset( $formulaPort );
 
 $GLOBALS['wgSimpleSAMLphp_InstallDir'] = '/app/simplesamlphp';
 
