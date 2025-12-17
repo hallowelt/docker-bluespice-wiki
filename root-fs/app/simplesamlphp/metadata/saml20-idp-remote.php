4f9fbee8c5fa0301d938d2e175189df6f7ca0acf
Robert Vogel <vogel@hallowelt.biz>
2025-12-17 09:15:22 +0100

    Move to dedicated version branch

--- root-fs/app/simplesamlphp/metadata/saml20-idp-remote.php
+++
@@ -1,7 +0,0 @@
-<?php
-
-$idpRemoteMetaData = require( __DIR__ . '/../config/_bluespice-saml20-idp-remote-meta.php' );
-
-$metadata[ $idpRemoteMetaData['entityid'] ] = $idpRemoteMetaData;
-
-unset( $idpRemoteMetaData );
