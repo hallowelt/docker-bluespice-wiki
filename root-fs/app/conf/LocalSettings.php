1f21a20fa9f3319a8cde55b5dc50efea94e8376b
Vova <matviienko@hopintaxi.com>
2026-07-07 19:20:21 +0200

    Revert "restore settings"
    This reverts commit cac5db69dff0de9cf79e02fa0882960755161b25.

--- root-fs/app/conf/LocalSettings.php
+++ root-fs/app/conf/LocalSettings.php
@@ -140,8 +140,11 @@ if ( getenv( 'EDITION' ) !== 'free' ) {
 	$GLOBALS['wgDrawioEditorBackendUrl'] = bsAssembleURL( 'DIAGRAM_PROTOCOL', 'DIAGRAM_HOST', 'DIAGRAM_PORT', 'DIAGRAM_PATH' );
 }
 
-$GLOBALS['wgMathValidModes'] = [ 'mathml' ];
-$GLOBALS['wgDefaultUserOptions']['math'] = 'mathml';
+// $GLOBALS['wgMathValidModes'] = [ 'mathml' ];
+// $GLOBALS['wgDefaultUserOptions']['math'] = 'mathml';
+$GLOBALS['wgMathValidModes'] = [ 'native' ];
+$GLOBALS['wgDefaultUserOptions']['math'] = 'native';
+$GLOBALS['wgDefaultUserOptions']['math-popups'] = '1';
 $GLOBALS['wgMaxShellMemory'] = 1228800;
 $GLOBALS['wgHiddenPrefs'][] = 'math';
 // We don't use the `MathMathML` renderer, but `MathMathMLCli`,
